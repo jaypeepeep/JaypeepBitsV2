ed15e5892a16ac039e33dc8df6ab3514fb54e603
JP <patrick.zutagal@gmail.com>
2025-06-05 00:40:27 +0800

    Feat: Add logo in link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'JaypeepBits | Full-Stack Developer Portfolio',
+ description: "The personal portfolio of John Patrick Lagatuz (JP), a backend-focused full stack developer passionate about building smart, user-friendly web apps with PHP, Node.js, React, and MySQL. Explore my work, skills, and what I’ve enjoyed creating.",
+ generator: 'Next.js',
 }
 
 export default function RootLayout({
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        <head>
+    <link rel="icon" href="/images/branding/logo.png" />
+  </head>
       <body>{children}</body>
     </html>
   )
